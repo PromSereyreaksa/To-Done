7086c6d39c5dfdfff29489e835fdad6cb1a937d2
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2025-03-31 02:30:58 +0700

    x

--- To-Done/vite.config.js
+++ To-Done/vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),     tailwindcss()],
+  base:"/To_Done ",
+  
+    build: {
+      outDir: 'dist/client',  // Make sure this is correctly configured
+    },
+  
+  
 
 })
